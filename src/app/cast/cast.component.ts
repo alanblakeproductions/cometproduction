e1560235ef3c3d80cf0ea1e2482c090654ffcaad
Alan Blake <alanblakeproductions@gmail.com>
2024-05-04 12:46:03 -0500

    Sean McGinley

--- src/app/cast/cast.component.ts
+++ src/app/cast/cast.component.ts
@@ -33,7 +33,7 @@ export class CastComponent {
       ])
 
       this.typeToMembers.set("Crew", [
-          {firstName: "Sean", lastName: "McGinley", roles: ["Sound Design"], image: "https://placehold.co/400x400"},
+          {firstName: "Sean", lastName: "McGinley", roles: ["Sound Design"], image: "assets/images/sean_mcginley.jpg"},
           {firstName: "Emmy", lastName: "Masteller", roles: ["Photography"], image: "assets/images/emmy_masteller.jpg"},
           {firstName: "Charlie", lastName: "Thomson", roles: ["Graphic Design"], image: "assets/images/charlie_thomson.jpg"},
           {firstName: "Alan", lastName: "Blake", roles: ["Set Construction"], image: "assets/images/alan_blake.jpg"},
