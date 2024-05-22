6022f46848a924e2e74a7f6e3e1d479381833fb4
Alan Blake <alanblakeproductions@gmail.com>
2024-05-22 12:59:38 -0500

    51% of goal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,7 +13,7 @@ import { Address, Showrun, Showtime } from './../util/models';
 })
 export class HomeComponent {
 
-  FUNDRAISING_CURRENT: number = 812;
+  FUNDRAISING_CURRENT: number = 1024;
   FUNDRAISING_GOAL: number = 2000;
   FUNDRAISING_CURRENT_PERCENT: number = Math.floor(100 * this.FUNDRAISING_CURRENT / this.FUNDRAISING_GOAL);
 
